ecba5d4a190484f52d1e37ec951bd2c6532b2c9e
zhujun <jun.zhu@eayun.com>
2016-07-08 17:49:09 +0800

    worker 抓取数据时，user-agent随机

--- spider-worker/src/main/java/com/zhujun/spider/worker/fetch/JavaUrlContentFetcher.java
+++ spider-worker/src/main/java/com/zhujun/spider/worker/fetch/JavaUrlContentFetcher.java
@@ -6,6 +6,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +23,29 @@ public class JavaUrlContentFetcher implements ContentFetcher {
 	
 	private final static JavaUrlContentFetcher INSTANCE = new JavaUrlContentFetcher();
 	
+	private final static String[] USER_AGENTS = new String[]{
+			// firefox
+			"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0",
+			"Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070803 Firefox/1.5.0.12",
+			
+			// chrome
+			"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36",
+			"Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.13",
+			
+			// ie
+			"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)",
+			
+			// opera
+			"Opera/9.27 (Windows NT 5.2; U; zh-cn)",
+			"Mozilla/5.0 (Macintosh; PPC Mac OS X; U; en) Opera 8.0",
+			
+			// safari
+			"Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Version/3.1 Safari/525.13",
+			"Mozilla/5.0 (iPhone; U; CPU like Mac OS X) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/4A93 Safari/419.3",
+			
+			
+	};
+	
 	public static JavaUrlContentFetcher getInstance() {
 		return INSTANCE;
 	}
@@ -34,9 +58,12 @@ public class JavaUrlContentFetcher implements ContentFetcher {
 		try {
 			URL urlObj = new URL(url);
 			connection = (HttpURLConnection)urlObj.openConnection();
-			connection.disconnect();
 			connection.setDoOutput(false);
 			connection.setDoInput(true);
+			
+			connection.setRequestProperty("User-Agent", USER_AGENTS[RandomUtils.nextInt(0, USER_AGENTS.length)]);
+			connection.setRequestProperty("Cache-Control", "max-age=0");
+			connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 			
 			int httpRespCode = connection.getResponseCode();
 			if (httpRespCode >= 400) {
