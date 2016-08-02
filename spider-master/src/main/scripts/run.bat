3e9923570d6f3551ad61d2b04e580afa1f5a7828
zhujun <jun.zhu@eayun.com>
2016-08-02 11:13:38 +0800

    修改bat启动脚本

--- spider-master/src/main/scripts/run.bat
+++ spider-master/src/main/scripts/run.bat
@@ -1,7 +1,7 @@
 :: bat for run spider master
 echo off
 
-set base_dir=%~dp0..\
+set base_dir=%~dp0../
 cd %base_dir%
 
-java -cp .;./lib/* -Dfile.encoding=UTF-8 com.zhujun.spider.master.Startup
+start java -cp %base_dir%;%base_dir%/lib/* -Dfile.encoding=UTF-8 com.zhujun.spider.master.Startup
--- spider-worker/src/main/scripts/run.bat
+++ spider-worker/src/main/scripts/run.bat
@@ -1,7 +1,7 @@
 :: bat for run spider worker
 echo off
 
-set base_dir=%~dp0..\
+set base_dir=%~dp0../
 cd %base_dir%
 
-java -cp .;./lib/* -Dfile.encoding=UTF-8 com.zhujun.spider.worker.Startup 127.0.0.1 8619
+start java -cp %base_dir%;%base_dir%/lib/* -Dfile.encoding=UTF-8 com.zhujun.spider.worker.Startup 127.0.0.1 8619
