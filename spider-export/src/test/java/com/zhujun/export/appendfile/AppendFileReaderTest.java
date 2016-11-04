6643c9bc048c168cf37961e33e5e59aaf6c06be9
Administrator <Administrator@SC-201607212125>
2016-11-04 23:14:03 +0800

    AppendFileReader增加读文件数据测试用例

--- spider-export/src/test/java/com/zhujun/export/appendfile/AppendFileReaderTest.java
+++ spider-export/src/test/java/com/zhujun/export/appendfile/AppendFileReaderTest.java
@@ -34,5 +34,34 @@ public class AppendFileReaderTest {
 			}
 		}
 	}
+	
+	
+	@Test
+	public void testReadMetaAndData() throws FileNotFoundException {
+		AppendFileReader reader = new AppendFileReader("D:/spider/meituan_clone/data-20161104220641");
+		try {
+			MetaData metaData = null;
+			while (true) {
+				metaData = reader.readMetaData();
+				if (metaData == null) {
+					break;
+				}
+				
+				System.out.println("Meta: " + metaData.getUrl() + " | " + metaData.getSize() + " | " 
+						+ metaData.getContentType() + " | " + metaData.getFetchTime());
+				System.out.println(new String(reader.readFileData()));
+				System.out.println();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
