14988813dd635f94d38fd2e1549a3052a6bac8a5
zhujun <juncke@163.com>
2016-11-04 23:09:36 +0800

    实现appendfile的文件读取

--- spider-export/src/main/java/com/zhujun/export/appendfile/AppendFileReader.java
+++ spider-export/src/main/java/com/zhujun/export/appendfile/AppendFileReader.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  * AppendFile读取器
@@ -21,6 +23,8 @@ public class AppendFileReader implements Closeable {
 	
 	private final static String START_LINE_FLAG = "SPIDER";
 	private final static String EMPTY_LINE = "";
+	
+	private final static SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
 	private String filePath;
 	private RandomAccessFile randomAccessFile;
@@ -29,6 +33,16 @@ public class AppendFileReader implements Closeable {
 	 * 目前读取状态
 	 */
 	private int status = STATUS_READ_SPIDER;
+	
+	/**
+	 * 当前文件大小
+	 */
+	private long currentFileSize = 0;
+	
+	/**
+	 * 是否已经读取文件数据
+	 */
+	private boolean isReadFile = false;
 	
 	public AppendFileReader(String filePath) throws FileNotFoundException {
 		this.filePath = filePath;
@@ -51,6 +65,11 @@ public class AppendFileReader implements Closeable {
 	 * @throws IOException 
 	 */
 	public MetaData readMetaData() throws IOException {
+		if (status == STATUS_READ_FILE && !isReadFile && currentFileSize > 0) {
+			// 有文件数据未读, 跳过文件数据
+			randomAccessFile.skipBytes((int)currentFileSize);
+		}
+		
 		status = STATUS_READ_SPIDER; // 重置状态
 		MetaData metaData = null;
 		String line = null;
@@ -75,13 +94,22 @@ public class AppendFileReader implements Closeable {
 				if (line == null || EMPTY_LINE.equals(line)) {
 					// header结束
 					status = STATUS_READ_FILE;
+					
+					// 设置后续文件数据
+					isReadFile = false;
+					currentFileSize = metaData.getSize();
+					
 					return metaData;
 				}
 				
 				// 解析header
-				String[] headerPair = line.split(";", 2);
+				String[] headerPair = line.split(":", 2);
+				if (headerPair.length != 2) {
+					continue;
+				}
 				
-				
+				// 填充metadata值
+				fillMetaData(metaData, headerPair[0].trim(), headerPair[1].trim());
 			}
 		}
 		
@@ -89,15 +117,51 @@ public class AppendFileReader implements Closeable {
 	}
 	
 	/**
+	 * 使用appendfile中数据header填充metadata
+	 * 
+	 * @param metaData
+	 * @param headerName
+	 * @param headerValue
+	 */
+	private void fillMetaData(MetaData metaData, String headerName, String headerValue) {
+		if ("Url".equals(headerName)) {
+			metaData.setUrl(headerValue);
+		} else if ("Content-Length".equals(headerName)) {
+			metaData.setSize(Long.valueOf(headerValue));
+		} else if ("ContentType".equals(headerName)) {
+			metaData.setContentType(headerValue);
+		} else if ("Time".equals(headerName)) {
+			try {
+				metaData.setFetchTime(TIME_FORMAT.parse(headerValue));
+			} catch (ParseException e) {
+				// do nothing
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
 	 * 读取当前元数据的文件数据
 	 * 
 	 * @author zhujun
 	 * @date 2016年11月4日
 	 *
 	 * @return
+	 * @throws IOException 
 	 */
-	public byte[] readFileData() {
-		return null;
+	public byte[] readFileData() throws IOException {
+		if (status != STATUS_READ_FILE || currentFileSize == 0 || isReadFile) {
+			return null;
+		}
+		
+		byte data[] = new byte[(int)currentFileSize];
+		randomAccessFile.read(data);
+		
+		status = STATUS_READ_SPIDER; // 设置下一阶段状态
+		isReadFile = true;
+		currentFileSize = 0;
+		
+		return data;
 	}
 	
 	@Override
---
+++ spider-export/src/test/java/com/zhujun/export/appendfile/AppendFileReaderTest.java
@@ -0,0 +1,38 @@
+package com.zhujun.export.appendfile;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class AppendFileReaderTest {
+
+	@Test
+	public void testReadMetaData() throws FileNotFoundException {
+		AppendFileReader reader = new AppendFileReader("D:/spider/meituan_clone/data-20161104220641");
+		try {
+			MetaData metaData = null;
+			while (true) {
+				metaData = reader.readMetaData();
+				if (metaData == null) {
+					break;
+				}
+				
+				System.out.println(metaData.getUrl() + " | " + metaData.getSize() + " | " 
+						+ metaData.getContentType() + " | " + metaData.getFetchTime());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
